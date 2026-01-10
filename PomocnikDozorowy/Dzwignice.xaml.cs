0453a15040a7dc8bba02ad74e90cbdeab7e33dd0
Kudłaty <kudlat.g4ming@gmail.com>
2026-01-10 21:32:43 +0100

    Nowe okno i klasa

---
+++ PomocnikDozorowy/Dzwignice.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PomocnikDozorowy
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Dzwignice.xaml
+    /// </summary>
+    public partial class Dzwignice : Window
+    {
+        public Dzwignice()
+        {
+            InitializeComponent();
+        }
+    }
+}
